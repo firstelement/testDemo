7d8e68bf1f2d174858fd8698d57d12b1333e9e35
arcado <arcado@arcado-ThinkPad-T440s>
2019-11-14 13:47:49 +0800

    test6

--- src/test/java/com/sunmi/test/tetstplay.java
+++ src/test/java/com/sunmi/test/tetstplay.java
@@ -4,14 +4,16 @@ import static org.testng.Assert.assertTrue;
 
 import org.testng.annotations.Test;
 
+import com.sunmi.base.testplay;
+
 public class tetstplay {
 
 	@Test
 	public void erro() {
-		assertTrue(false);
+		assertTrue(new testplay().err());
 	}
 	@Test
 	public void erro2() {
-		assertTrue(true);
+		assertTrue(new testplay().err2());
 	}
 }
